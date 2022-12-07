d48c902292c57f4fef0127ced91b6a14ba6b45d3
David <dholmes3031@gmail.com>
2022-12-07 13:22:36 +0000

    errors handled for invalid topics/path

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import Navbar from "./components/Navbar";
 import ArticleList from "./components/ArticleList";
 import SingleArticle from "./components/SingleArticle";
 import { UserContext } from "./contexts/userContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Topics from "./components/Topics";
+import InvalidTopic from "./components/InvalidTopic";
+import { getTopics } from "./api/api";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [userStatus, setUserStatus] = useState(false);
+  const [currentTopic, setCurrentTopic] = useState();
+  const [validTopics, setValidTopics] = useState([]);
+
+  useEffect(() => {
+    getTopics().then((topics) => {
+      const valid = topics.map((topic) => {
+        return topic.slug;
+      });
+      setValidTopics(valid);
+      setLoading(false);
+    });
+  }, []);
 
   const [user, setUser] = useState({
     username: "cooljmessy",
@@ -17,21 +32,35 @@ function App() {
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
   });
-
-  return (
-    <UserContext.Provider value={{ user, setUser, userStatus, setUserStatus }}>
-      <div className="App">
-        <Header />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ArticleList />} />
-          <Route path="/topics" element={<Topics />} />
-          <Route path="/topics/:topic_slug" element={<ArticleList />} />
-          <Route path="/article/:article_id" element={<SingleArticle />} />
-        </Routes>
-      </div>
-    </UserContext.Provider>
-  );
+  if (!loading) {
+    return (
+      <UserContext.Provider
+        value={{ user, setUser, userStatus, setUserStatus }}
+      >
+        <div className="App">
+          <Header />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ArticleList />} />
+            <Route path="/topics" element={<Topics />} />
+            <Route
+              path="/topics/:topic_slug"
+              element={
+                validTopics.includes(currentTopic) ? (
+                  <ArticleList />
+                ) : (
+                  <InvalidTopic setCurrentTopic={setCurrentTopic} />
+                )
+              }
+            />
+            <Route path="/article/:article_id" element={<SingleArticle />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
+    );
+  } else {
+    return <h2>Loading...</h2>;
+  }
 }
 
 export default App;
---
+++ src/components/InvalidTopic.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Topics from "./Topics";
+
+function InvalidTopic({ setCurrentTopic }) {
+  const { topic_slug } = useParams();
+
+  useEffect(() => {
+    setCurrentTopic(topic_slug);
+  }, [setCurrentTopic, topic_slug]);
+
+  return (
+    <div>
+      <h1>This topic does not exist!</h1>
+      <h2>Please chose one from the list below</h2>
+      <Topics />
+    </div>
+  );
+}
+
+export default InvalidTopic;
